e38adf1ceaa0162ec07f7565b76b07fbd5c61641
szteffeff <72330991+szteffeff@users.noreply.github.com>
2022-01-10 13:21:52 -0800

    Update Entitiy.cpp

--- FalpGL/src/game/Entitiy.cpp
+++ FalpGL/src/game/Entitiy.cpp
@@ -64,3 +64,50 @@ Quad* Entity::get_quad()
 {
 	return &m_quad;
 }
+
+void Player::walk(float direction, float magnitude)
+{ /* This should all be in Player::tick() */
+	if (m_quad.center().x + momentum[0] < -400.0f || m_quad.center().x + momentum[0] > 400.0f)
+		player_screen_position[0] += momentum[0]
+	else
+		m_quad.translate(momentum[0], 0.0f)
+		
+		
+	if (m_quad.center().y + momentum[1] < -400.0f || m_quad.center().y + momentum[1] > 400.0f)
+		player_screen_position[1] += momentum[1]
+	else
+		m_quad.translate(0.0f, momentum[1])
+		
+		
+	momentum[0] *= 0.9;
+	momentum[1] *= 0.9;
+
+	if (sqrt(momentum[0] * momentum[0] + momentum[1] * momentum[1]) > 0.125)
+	{
+		float dir = (atan2(momentum[1], momentum[0]));
+		if (dir < 0) { dir += 2 * 3.14159; }
+		unsigned int diri = (unsigned int)round(dir / 3.14159 * 2);
+		set_animation((diri == 0) ? 4 : diri);
+	}
+	else
+	{
+		set_animation(0);
+	}
+
+	animations[active_animation].tick();
+}
+
+glm::mat4* Player::get_trans_matrix()
+{
+	return(&player_transform_matrix);
+}
+
+const float Player::screen_pos_x()
+{
+	return(position_on_screen[0]);	
+}
+
+const float Player::screen_pos_y()
+{
+	return(position_on_screen[1]);	
+}
